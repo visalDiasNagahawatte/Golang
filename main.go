e2275e617e4a54f6cb9c4e4d943a94a922044869
Visal Nagahawatte <132425926+visalDiasNagahawatte@users.noreply.github.com>
2024-12-02 14:46:20 +0530

    feat:categorize using variables

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
+
 import "fmt"
 func main(){
-	fmt.Println("Hello World!")
+    var firstTask = "Submit a report"
+	var secondTask = "Office meeting"
+	var thirdTask = "Go to Supermarket"
+	var fourthTask =  "Get a haircut"
+	var fifthTask = "Go on a trip"
+ 
+	fmt.Println("My To-do List")
+	fmt.Println()
+	fmt.Println("Work")
+	fmt.Println(firstTask)
+	fmt.Println(secondTask)
+
+	fmt.Println()
+	fmt.Println("Family")
+	fmt.Println(thirdTask)
+	fmt.Println()
+	fmt.Println("Life")
+	fmt.Println(fourthTask)
+	fmt.Println(fifthTask)
+	fmt.Println()
 }

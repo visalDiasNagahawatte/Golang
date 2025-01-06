cc602b2bbb4ee69db605e652ff27b04b97fec055
Visal Nagahawatte <132425926+visalDiasNagahawatte@users.noreply.github.com>
2025-01-06 08:10:03 +0530

    feat: create  adiitional functions

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import "fmt"
+
+
+
 func main(){
-    var firstTask = "Submit a report"
+
+	var firstTask = "Submit a report"
 	var secondTask = "Office meeting"
 	var thirdTask = "Go to Supermarket"
 	var fourthTask =  "Get a haircut"
 	var fifthTask = "Go on a trip"
+	
+	var taskItems =[]string {firstTask,secondTask,thirdTask,fourthTask,fifthTask}
+
+    fmt.Println("Welcome to To-Do List App")
+	fmt.Println()
  
+	printTasks(taskItems)
+	taskItems= addTasks(taskItems,"Go for a run")
+	
+	fmt.Println("updated list")
+	printTasks(taskItems)
+
+}
+
+func printTasks(taskItems []string){
 	fmt.Println("My To-do List")
 	fmt.Println()
-	fmt.Println("Work")
-	fmt.Println(firstTask)
-	fmt.Println(secondTask)
 
-	fmt.Println()
-	fmt.Println("Family")
-	fmt.Println(thirdTask)
-	fmt.Println()
-	fmt.Println("Life")
-	fmt.Println(fourthTask)
-	fmt.Println(fifthTask)
-	fmt.Println()
+	for index, task := range taskItems{
+		fmt.Printf("%d. %s\n",index+1,task)
+
+	} 
+}
+
+func addTasks(taskItems []string, newTask string) []string{
+     var updatedTask = append(taskItems,newTask)
+	 return updatedTask
 }
